67e721563e86ecd65a669151cf4d271b22c31653
Nohbdy Ahtall <1641861-Nohbdy@users.noreply.gitlab.com>
2026-04-25 23:51:49 -0700

    Tooltip theme

--- experiments/menu_gradient_thing.gd
+++ experiments/menu_gradient_thing.gd
@@ -1,11 +1,25 @@
 extends TextureRect
 
+enum AXIS {
+	x,
+	y
+}
+
+## Changes that... thing... >.>
+func change_that_thing(point: int, axis: AXIS, value: float) -> void:
+	var curve = texture.curve as Curve
+	var target : float
+	if axis == AXIS.x:
+		target = curve.get_point_position(point).x
+	elif axis == AXIS.y:
+		target = curve.get_point_position(point).y
+	
+	var new_point = (target + value) / 16
+	curve.set_point_value(point, new_point)
+
+
 func _on_h_slider_value_changed(value: float) -> void:
-	var c = texture.curve as Curve
-	var new_point_x = (c.get_point_position(0).x + value) / 16
-	c.set_point_value(0, new_point_x)
+	change_that_thing(0, AXIS.x, value)
 
 func _on_h_slider_value_changed_2(value: float) -> void:
-	var c = texture.curve as Curve
-	var new_point_x = (c.get_point_position(1).x + value) / 16
-	c.set_point_value(1, new_point_x)
+	change_that_thing(1, AXIS.x, value)
